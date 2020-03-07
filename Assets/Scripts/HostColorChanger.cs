64239fd8a23e61f518074cc3af0d33c095605ee7
stayseek <stayseek0@gmail.com>
2020-03-07 17:09:10 +0300

    HW8_test_ready

---
+++ Assets/Scripts/HostColorChanger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostColorChanger : NetworkBehaviour
+{
+    [SerializeField] Renderer _renderer;
+    [SerializeField] Material _clientMaterial;
+    [SerializeField] Material _hostMaterial;
+
+    [SyncVar] bool _isHost;
+
+    private void Update()
+    {
+        if (isServer)
+        {
+            if (isLocalPlayer)
+            {
+                _isHost = true;
+            }
+            else
+            {
+                _isHost = false;
+            }
+        }
+        if (_isHost)
+        {
+            _renderer.material = _hostMaterial;
+        }
+        else
+        {
+            _renderer.material = _clientMaterial;
+        }
+    }
+}
---
+++ Assets/Scripts/SpiritsMotor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpiritsMotor : NetworkBehaviour
+{
+
+    [SerializeField] float _speed;
+    Transform _transform;
+    Quaternion _oldRot;
+
+    void Start()
+    {
+        _transform = transform;
+    }
+
+    void Update()
+    {
+        if (hasAuthority)
+        {
+            _oldRot = _transform.localRotation;
+            _transform.localRotation = Quaternion.Euler(_oldRot.eulerAngles.x, _oldRot.eulerAngles.y + _speed * Time.deltaTime, _oldRot.eulerAngles.z);
+        }
+    }
+}
