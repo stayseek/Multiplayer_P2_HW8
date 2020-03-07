de6aa3ae218348c982897980ec1c24413e3cbab2
stayseek <stayseek0@gmail.com>
2020-03-07 17:38:26 +0300

    HW8_done

---
+++ Assets/Scripts/Controllers/MyNetworkMigrationManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkMigrationManager : NetworkMigrationManager
+{
+    protected override void OnServerHostShutdown()
+    {
+        Debug.Log("Server was shutdowned.");
+        base.OnServerHostShutdown();
+    }
+
+    protected override void OnServerReconnectPlayer(NetworkConnection newConnection, GameObject oldPlayer, int oldConnectionId, short playerControllerId)
+    {
+        Debug.Log($"Player was {oldPlayer.name} reconnected.");
+        base.OnServerReconnectPlayer(newConnection, oldPlayer, oldConnectionId, playerControllerId);
+    }
+}
